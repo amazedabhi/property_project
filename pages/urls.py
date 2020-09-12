45171f0e1938be97cf5667706ea4d140277ea3db
justtabhi <abhi.in12@gmail.com>
2020-09-12 19:26:36 +0530

    Added Pages Templates & Base Layout

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from pages import views
 
 urlpatterns =[
-    path('',views.index),
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+
 ]
